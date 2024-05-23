b9ba724bc08ab8ceb7222afa2349342586aedddd
asitm55 <167652844+asitm55@users.noreply.github.com>
2024-05-23 22:33:02 +0200

    Update datapipeline.py

--- project/datapipeline.py
+++ project/datapipeline.py
@@ -1,1 +1,26 @@
+import os
+os.environ['KAGGLE_USERNAME'] = 'asit111' 
+os.environ['KAGGLE_KEY'] = '9b8b0b2238108048307e7fb9a882db93' 
 
+import kaggle
+import time
+
+dataset_links = ["https://www.kaggle.com/datasets/shrutibhargava94/india-air-quality-data",
+                 "https://www.kaggle.com/datasets/rohanrao/air-quality-data-in-india",
+                 "https://www.kaggle.com/datasets/abhisheksjha/time-series-air-quality-data-of-india-2010-2023",
+                 "https://www.kaggle.com/datasets/fedesoriano/air-quality-data-in-india",
+                 "https://www.kaggle.com/datasets/neomatrix369/air-quality-data-in-india-extended"]
+
+for dataset in dataset_links:
+    folder_name = f"../data/{'-'.join(dataset.split('/')[-2:])}"
+    dataset = '/'.join(dataset.split('/')[-2:])
+
+    print(f'Creating folder at {folder_name}')
+    os.mkdir(folder_name)
+
+    print(f'Downloading the {dataset} dataset')
+
+    kaggle.api.dataset_download_files(dataset, path=folder_name, unzip=True)
+    time.sleep(1)
+
+print('Done')
